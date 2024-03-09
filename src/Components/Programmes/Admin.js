66571248c9727ab3f91f743066667d14223bbc6e
Sw_abah <ahmedswabah922@gmail.com>
2024-03-10 01:15:10 +0530

    added Expence Detiles

--- src/Components/Programmes/Admin.js
+++ src/Components/Programmes/Admin.js
@@ -1,4 +1,4 @@
-import { collection, onSnapshot, orderBy, query } from 'firebase/firestore';
+import { addDoc, collection, deleteDoc, getDocs, onSnapshot, orderBy, query, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { db } from '../../Firebase/Config';
 import datesfriuts from '../assets/images/datesfriuts.jpg'
@@ -8,10 +8,15 @@ import ParticipateModal from '../assets/Challenges/ParticipateModal';
 import { FcCancel, FcCheckmark } from 'react-icons/fc';
 import { FaCircleArrowLeft } from 'react-icons/fa6';
 import { TECollapse } from "tw-elements-react";
+import { BsClipboard2PlusFill } from "react-icons/bs";
+import { RiDeleteBin6Line } from "react-icons/ri";
 
 function Admin() {
     const [Participates, setParticipates] = useState([]);
     const [fetchLoading, setFetchLoading] = useState(true);
+    const [ExpenceCash, setExpenceCash] = useState('');
+    const [Expence, setExpence] = useState('');
+    const [ExpenceList, setExpenceList] = useState([]);
     const [showModalId, setShowModalId] = useState(null);
     const navigate = useNavigate();
 
@@ -53,7 +58,7 @@ function Admin() {
                             ...doc.data(),
                         }));
                         setParticipates(Participates);
-                        setFetchLoading(false);
+                        setFetchLoading(false)
                     }
                 );
 
@@ -63,9 +68,69 @@ function Admin() {
             }
         };
 
+        const fetchExpenceData = async () => {
+            try {
+                const userCollectionRef = collection(db, 'Expences');
+                const unsubscribe = onSnapshot(
+                    query(userCollectionRef, orderBy('Date', 'desc')), // or 'desc' based on your requirement
+                    (querySnapshot) => {
+                        const ExpenceList = querySnapshot.docs.map((doc) => ({
+                            id: doc.id,
+                            ...doc.data(),
+                        }));
+                        setExpenceList(ExpenceList);
+                    }
+                );
+
+                return unsubscribe;
+            } catch (error) {
+                alert(error);
+            }
+        };
+
+        fetchExpenceData();
         fetchData();
     }, []);
 
+    const handleAddExpence = async (e) => {
+        e.preventDefault();
+        try {
+            // Perform Expence Detailes
+            const path = collection(db, 'Expences');
+            await addDoc(path, {
+                Expence,
+                ExpenceCash,
+                Date: new Date()
+            });
+            setTimeout(() => {
+                setExpence('')
+                setExpenceCash('')
+            }, 500);
+        } catch (error) {
+            alert(error.message);
+        }
+    };
+
+    const deleteExpenceWithoutUid = async (par) => {
+        try {
+          const querySnapshot = await getDocs(
+            query(collection(db, 'Expences'),
+              where('Expence', '==', par.Expence),
+              where('ExpenceCash', '==', par.ExpenceCash),
+              where('Date', '==', par.Date)
+            )
+          );
+    
+          if (!querySnapshot.empty) {
+            const documentToDelete = querySnapshot.docs[0];
+            await deleteDoc(documentToDelete.ref);
+          } else {
+            console.log('No matching documents found.');
+          }
+        } catch (error) {
+          console.error('Error deleting document:', error);
+        }
+      };
 
     return (
         <div className='flex flex-col w-full h-full'>
@@ -83,8 +148,70 @@ function Admin() {
                 </div>
             </Link>
             <div className='flex flex-col w-full h-full gap-5 py-14 lg:py-20 p-7 md:px-20 lg:px-32 xl:px-44'>
-                <div>
-                    <p>Totel : </p>
+                <div className='flex flex-col pb-10'>
+                    <div onClick={() => handleClick("expence")}
+                        aria-expanded="true"
+                        aria-controls="expence" className='p-2 px-6 rounded-t font-medium lg:text-xl bg-[#071a2b] flex justify-between items-center text-[#d3e3fd]'>
+                        <p>Challenge Expences</p>
+                        <span>{ExpenceList.length}</span>
+                    </div>
+                    <TECollapse
+                        show={activeElement === "expence"}
+                        className="!mt-0 !rounded-b-none !shadow-none"
+                    >
+                        <div className="overflow-x-auto">
+                            <div className="inline-block min-w-full align-middle">
+                                <div className="overflow-hidden border rounded-b-lg shadow">
+                                    <form onSubmit={handleAddExpence}>
+                                        <table className="min-w-full divide-y divide-gray-200">
+                                            <thead>
+                                                <tr>
+                                                    <th className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm">Expense</th>
+                                                    <th className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm">Cash</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody className="divide-y divide-gray-200">
+                                                {fetchLoading ? (
+                                                    <>
+                                                        <tr>
+                                                            <td className='w-full h-12 bg-gray-100 animate-pulse'></td>
+                                                            <td className='w-full h-12 bg-gray-100 animate-pulse'></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td className='w-full h-12 bg-gray-100 animate-pulse'></td>
+                                                            <td className='w-full h-12 bg-gray-100 animate-pulse'></td>
+                                                        </tr>
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <tr>
+                                                            <td className="px-6 py-4 whitespace-nowrap font-medium text-[#071a2b] capitalize">
+                                                                <input required className="w-full p-0 py-2 border-0 border-b border-gray-300 outline-none ring-0 placeholder:font-normal placeholder:opacity-50 placeholder:text-black " type="text" placeholder='Type Expense' name="contact" value={Expence} onChange={(e) => setExpence(e.target.value)} />
+                                                            </td>
+                                                            <td className="px-6 py-4 flex items-center justify-between whitespace-nowrap text-sm font-medium text-[#071a2b] capitalize">
+                                                                <input className='w-1/2' required placeholder='Cash' value={ExpenceCash} type='number' onChange={(e) => setExpenceCash(e.target.value)} />
+                                                                <button type="submit"><BsClipboard2PlusFill className='text-xl' /></button>
+                                                            </td>
+                                                        </tr>
+                                                        {ExpenceList.map((ex) => (
+                                                            <tr key={ex.id}>
+                                                                <td className="px-6 py-4 whitespace-nowrap font-medium text-[#071a2b] capitalize">{ex.Expence}</td>
+                                                                <td className="px-6 py-4 flex items-center justify-between whitespace-nowrap text-sm font-medium text-[#071a2b] capitalize">
+                                                                    <p>{ex.ExpenceCash}</p>
+                                                                    <button onClick={() => deleteExpenceWithoutUid(ex)}><RiDeleteBin6Line  className='text-xl' /></button>
+                                                                </td>
+                                                            </tr>
+                                                        ))}
+                                                    </>
+                                                )}
+                                            </tbody>
+                                        </table>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </TECollapse>
                 </div>
                 <div id='admin' className='grid items-start grid-cols-1 gap-5 lg:grid-cols-2'>
                     {Participates.filter((par) => par.Package === '1 kg' && par.PackageType === 'Primary').length > 0 && (
@@ -427,48 +554,3 @@ function Admin() {
 
 export default Admin
 
-
-// const ParticipateTable = ({ showPartiModal, fetchLoading, Participates, type }) => {
-//     return (
-//         <div className="overflow-x-auto ">
-//             <div className="inline-block min-w-full align-middle">
-//                 <div className="overflow-hidden border rounded-lg shadow ">
-//                     <table className="min-w-full divide-y divide-gray-200 ">
-//                         <thead className="">
-//                             <tr>
-//                                 <th scope="col" className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm ">Payment</th>
-//                                 <th scope="col" className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm ">Token</th>
-//                                 <th scope="col" className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm ">P.Type</th>
-//                                 <th scope="col" className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm ">Name</th>
-//                                 <th scope="col" className="px-6 py-3 text-xs font-medium uppercase text-start lg:text-sm ">View</th>
-//                             </tr>
-//                         </thead>
-//                         <tbody className="divide-y divide-gray-200 ">
-//                             {fetchLoading ? (
-//                                 <>
-//                                     <tr><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td></tr>
-//                                     <tr><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td></tr>
-//                                     <tr><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td></tr>
-//                                     <tr><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td></tr>
-//                                     <tr><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td><td className='w-full h-12 bg-gray-100 animate-pulse'></td></tr>
-//                                 </>
-//                             ) : <>
-//                                 {Participates.filter((par) => par.PackageType === type).map((par) => (
-//                                     <tr>
-//                                         <td class="px-6 py-4 whitespace-nowrap font-medium text-[#071a2b] capitalize ">{par?.isPaid ? (<FcCheckmark />) : (<FcCancel />)}</td>
-//                                         <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-[#071a2b] capitalize ">{par.Token}</td>
-//                                         <td className="px-6 py-4 text-sm capitalize whitespace-nowrap ">{(par?.PackageType === 'Primary') ? (<p className='text-green-500' >{par.PackageType}</p>) : (<p className='text-red-500' >{par.PackageType}</p>)}</td>
-//                                         <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-[#071a2b] capitalize ">{par.fullName}</td>
-//                                         <td className="px-6 py-4 text-sm font-medium whitespace-nowrap">
-//                                             <button onClick={() => showPartiModal(par.Token)} type="button" className="inline-flex items-center text-sm font-semibold text-blue-600 border border-transparent rounded-lg gap-x-2 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600">View</button>
-//                                         </td>
-//                                     </tr>
-//                                 ))}
-//                             </>}
-//                         </tbody>
-//                     </table>
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
